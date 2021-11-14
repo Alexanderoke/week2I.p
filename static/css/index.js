992a2c0b48d787fa328b21d0ab7028a8bf4ffbab
alexanderoke <alexander.oke@student.moringaschool.com>
2021-11-14 07:27:51 +0300

    new background

---
+++ static/css/index.js
@@ -0,0 +1,13 @@
+//month codes(odd days).
+const Mc1=1;
+const Mc2=4;
+const Mc3=4;
+const Mc4=0;
+const Mc5=2;
+const Mc6=5;
+const Mc7=0;
+const Mc8=3;
+const Mc9=6;
+const Mcx=1;
+const Mcxi=4;
+const Mcxii=6;
